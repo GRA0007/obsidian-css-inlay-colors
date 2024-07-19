33a9a841a853266df717612170d2637e95c2c02c
Benji Grant <benjamin.grantGRA0007@gmail.com>
2024-07-19 13:39:46 +1000

    Set up workflows

--- version-bump.mjs
+++ version-bump.mjs
@@ -6,9 +6,9 @@ const targetVersion = process.env.npm_package_version
 const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
 const { minAppVersion } = manifest
 manifest.version = targetVersion
-writeFileSync('manifest.json', JSON.stringify(manifest, null, 2))
+writeFileSync('manifest.json', `${JSON.stringify(manifest, null, 2)}\n`)
 
 // update versions.json with target version and minAppVersion from manifest.json
 const versions = JSON.parse(readFileSync('versions.json', 'utf8'))
 versions[targetVersion] = minAppVersion
-writeFileSync('versions.json', JSON.stringify(versions, null, 2))
+writeFileSync('versions.json', `${JSON.stringify(versions, null, 2)}\n`)
